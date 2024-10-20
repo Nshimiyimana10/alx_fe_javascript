1ae9f015f4507333dc39e4a4d20a7a26c120e725
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-20 04:12:52 -0700

    added new functions

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -7,16 +7,20 @@ const newQuote = document.getElementById("newQuoteText");
 const category = document.getElementById("newQuoteCategory");
 newQuote.addEventListener("click", addQuote);
 const quoteDisplay = document.getElementById("quoteDisplay");
-showRandomQuote(){
+
+function showRandomQuote(){
 const random = (document.getElementById("quoteDisplay").innerHTML = array)
 const myRandom = Math.random(random);
 return myRandom;
 }
 
-addQuoteForm(){
-
+function createAddQuoteForm(){
+  const ele1 = document.createElement("div");
+  const add = document.appendChild(ele1);
+  const input = document.createElement("input");
+  const add2 = document.appendChild(input)
 }
 
-addQuote(){
-
+function addQuote(){
+ createAddQuoteForm();
 }
