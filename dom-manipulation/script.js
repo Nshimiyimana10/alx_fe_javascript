2cea59bdbd114ca3a0c92a6bca3b149e2dcec289
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-20 04:23:58 -0700

    added new file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -24,3 +24,16 @@ function createAddQuoteForm(){
 function addQuote(){
  createAddQuoteForm();
 }
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+
+  <input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />
