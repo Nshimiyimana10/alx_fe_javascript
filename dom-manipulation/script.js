1d3e4a4a68e21ff649162ec38b678e743867deb0
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-20 04:06:31 -0700

    added DOM selection elements

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,3 +2,21 @@ const array = [{
     text: "",
     category:""
 }]
+
+const newQuote = document.getElementById("newQuoteText");
+const category = document.getElementById("newQuoteCategory");
+newQuote.addEventListener("click", addQuote);
+const quoteDisplay = document.getElementById("quoteDisplay");
+showRandomQuote(){
+const random = (document.getElementById("quoteDisplay").innerHTML = array)
+const myRandom = Math.random(random);
+return myRandom;
+}
+
+addQuoteForm(){
+
+}
+
+addQuote(){
+
+}
